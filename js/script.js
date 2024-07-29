2809578963d05c5de5ec5e5100b342f38b8caaa5
AjitPrasad9 <139619909+AjitPrasad9@users.noreply.github.com>
2024-07-29 21:21:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing", {
-    strings: ["", "Graphic Designer", "Web & App Designer", "UI / UX Designer", "Web Developer", "Video Editor", "YouTuber", "Software Developer"],
+    strings: ["", "Graphic Designer", "UI / UX Designer", "Web Developer", "Video Editor", "YouTuber", "Software Developer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
